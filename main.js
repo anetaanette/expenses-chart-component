e1bf54777daa7f3f1e7fdf82451acc35c517d999
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-07-02 21:00:06 +0200

    more changes

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 console.log("hfygf");
+
+/* const hiddenAmount = document.getElementsByClassName(".amount");
+  const column = document.getElementsByClassName(".column");
+
+  let j = hiddenAmount.length;
+  while (j++) {
+  let i = column.length;
+while (i++) {
+  column[i].addEventListener("mouseover", showAmount);
+   
+  function showAmount() {
+      hiddenAmount.style.display = 'block';
+  }
+  }
+  };
+  */ 
+
+
+
+
 // GETTING JSON DATA 
 async function loadNumbers() {
-    const response = await fetch('./data.json');
-    const numbers = await response.json();
+    const result = await fetch('./data.json');
+    const numbers = await result.json();
     console.log(numbers); 
   }
 
   console.log(loadNumbers());
+/*
 
-  
+const hiddenMon = document.querySelector(".amount-mon");
+const hiddenTue = document.querySelector(".amount-tue");
+const hiddenWed = document.querySelector(".amount-wed");
+const hiddenThu = document.querySelector(".amount-thu");
+const hiddenFri = document.querySelector(".amount-fri");
+const hiddenSat = document.querySelector(".amount-sat");
+const hiddenSun = document.querySelector(".amount-sun");
+
+hiddenMon.addEventListener("mouseover", showNumber);
+
+function showNumber() {
+   hiddenMon.innerText = obj.amount[0]
+}
+*/
+
+//document.querySelector(".amount-mon").innerHTML = obj.amount[0];
+
